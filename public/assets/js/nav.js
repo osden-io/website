adb8a1fe0b9f68a22569c2abf44c23bd37ebc73c
Joel Gregory <jgregors@gmail.com>
2016-01-18 17:53:06 +1100

    initial commit

---
+++ public/assets/js/nav.js
@@ -0,0 +1,1 @@
+$('.nav').find('a[href="' + location.pathname + '"]').parents('li').addClass('active');
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/about', function(req, res) {
+  res.render('about');
+});
+
+router.get('/contact', function(req, res) {
+  res.render('contact');
+});
+
+module.exports = router;
